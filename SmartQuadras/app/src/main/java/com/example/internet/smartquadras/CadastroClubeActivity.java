9d7375dda3e1bf53cf969f5318c47469db4a8563
luiz gonzaga <luizmessias13@gmail.com>
2017-09-25 22:46:14 -0300

    commit no android studio

---
+++ SmartQuadras/app/src/main/java/com/example/internet/smartquadras/CadastroClubeActivity.java
@@ -0,0 +1,13 @@
+package com.example.internet.smartquadras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroClubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_clube2);
+    }
+}
